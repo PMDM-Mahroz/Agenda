1adb27ec96eb7101cd00777ac3956cacc683fcdd
mahroz jawad <mahrozjawad.mj@hotmail.com>
2019-10-17 21:27:39 +0200

    Agenda Segunda Parte

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_add_new_contact);
 
         EditText editTextNombre = findViewById(R.id.editText);
         EditText editTextApellidos = findViewById(R.id.editText2);
